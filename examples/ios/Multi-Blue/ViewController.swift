d0647a0c0562e4f08547ded3c1dd8509ab684705
Andrew Reed <andrew_reed@hotmail.com>
2019-08-15 18:04:47 +0100

    Added call to library to scan

--- examples/ios/Multi-Blue/ViewController.swift
+++ examples/ios/Multi-Blue/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import library
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MultiBlue().scan()
     }
 
 
