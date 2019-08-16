639459d27982db7a5ba253fff3f3848fb141b790
Andrew Reed <andrew_reed@hotmail.com>
2019-08-16 18:39:42 +0100

    Added android

--- examples/android/src/main/java/dev/bluefalcon/MainActivity.kt
+++ examples/android/src/main/java/dev/bluefalcon/MainActivity.kt
@@ -1,13 +1,22 @@
 package dev.bluefalcon
 
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        BlueFalcon.scan()
+
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        //bluetoothManager.adapter
+        //bluetoothManager.adapter
+        //BlueFalcon.scan()
     }
 }
