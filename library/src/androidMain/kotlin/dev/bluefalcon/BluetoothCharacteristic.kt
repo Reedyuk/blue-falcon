cd7315f899bf1731db4435897cdf4f5ce30d18bb
Andrew Reed <andrew_reed@hotmail.com>
2024-06-11 17:21:19 +0100

    Made android parcelable

--- library/src/androidMain/kotlin/dev/bluefalcon/BluetoothCharacteristic.kt
+++ library/src/androidMain/kotlin/dev/bluefalcon/BluetoothCharacteristic.kt
@@ -2,10 +2,11 @@ package dev.bluefalcon
 
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothGattDescriptor
+import android.os.Parcel
+import android.os.Parcelable
 import kotlinx.coroutines.flow.MutableStateFlow
-import java.nio.charset.Charset
 
-actual class BluetoothCharacteristic(val characteristic: BluetoothGattCharacteristic) {
+actual class BluetoothCharacteristic(val characteristic: BluetoothGattCharacteristic) : Parcelable {
     actual val name: String?
         get() = characteristic.uuid.toString()
     actual val value: ByteArray?
@@ -14,6 +15,31 @@ actual class BluetoothCharacteristic(val characteristic: BluetoothGattCharacteri
         get() = characteristic.descriptors
 
     internal actual val _descriptorsFlow = MutableStateFlow<List<BluetoothCharacteristicDescriptor>>(emptyList())
+
+    constructor(parcel: Parcel) : this(
+        requireNotNull(parcel.readParcelable(BluetoothGattCharacteristic::class.java.classLoader))
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(characteristic, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<BluetoothCharacteristic> {
+        override fun createFromParcel(parcel: Parcel): BluetoothCharacteristic {
+            return BluetoothCharacteristic(parcel)
+        }
+
+        override fun newArray(size: Int): Array<BluetoothCharacteristic?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
 }
 
 actual typealias BluetoothCharacteristicDescriptor = BluetoothGattDescriptor
--- library/src/androidMain/kotlin/dev/bluefalcon/BluetoothPeripheral.kt
+++ library/src/androidMain/kotlin/dev/bluefalcon/BluetoothPeripheral.kt
@@ -1,9 +1,11 @@
 package dev.bluefalcon
 
 import android.bluetooth.BluetoothDevice
+import android.os.Parcel
+import android.os.Parcelable
 import kotlinx.coroutines.flow.MutableStateFlow
 
-actual class BluetoothPeripheral(val bluetoothDevice: BluetoothDevice) {
+actual class BluetoothPeripheral(val bluetoothDevice: BluetoothDevice) : Parcelable {
     actual val name: String?
         get() = bluetoothDevice.name ?: bluetoothDevice.address
     actual val services: List<BluetoothService>
@@ -14,4 +16,27 @@ actual class BluetoothPeripheral(val bluetoothDevice: BluetoothDevice) {
     actual var rssi: Float? = null
 
     internal actual val _servicesFlow = MutableStateFlow<List<BluetoothService>>(emptyList())
+
+    constructor(parcel: Parcel) : this(requireNotNull(parcel.readParcelable(BluetoothDevice::class.java.classLoader))) {
+        rssi = parcel.readValue(Float::class.java.classLoader) as? Float
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(bluetoothDevice, flags)
+        parcel.writeValue(rssi)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<BluetoothPeripheral> {
+        override fun createFromParcel(parcel: Parcel): BluetoothPeripheral {
+            return BluetoothPeripheral(parcel)
+        }
+
+        override fun newArray(size: Int): Array<BluetoothPeripheral?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
