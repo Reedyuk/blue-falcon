68a923222ce86980e3bb4bf54eeadec0896a94ee
Andrew Reed <andrew_reed@hotmail.com>
2026-04-12 09:45:27 +0100

    chore: remove Maven publishing configuration from root module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
     kotlin("multiplatform") version "2.3.0"
     id("com.android.library")
-    id("com.vanniktech.maven.publish") version "0.34.0"
-    id("signing")
+    // Root module should not publish - only submodules (core, engines, plugins, legacy) publish
+    // This prevents duplicate artifact IDs with the legacy module
 }
 
 repositories {
@@ -105,59 +105,6 @@ kotlin {
     }
 }
 
-mavenPublishing {
-    publishToMavenCentral(automaticRelease = true)
-    signAllPublications()
-
-    coordinates(
-        groupId = group,
-        artifactId = libraryName,
-        version = version
-    )
-
-    pom {
-        name.set(group)
-        description.set(projectDescription)
-        url.set(projectGithubUrl)
-
-        licenses {
-            license {
-                name.set("MIT License")
-                url.set("http://opensource.org/licenses/MIT")
-            }
-        }
-
-        developers {
-            developer {
-                id.set(developerId)
-                name.set(developerName)
-                email.set(developerEmail)
-            }
-        }
-
-        scm {
-            url.set(projectGithubUrl)
-            connection.set(projectGithubSCM)
-            developerConnection.set(projectGithubSCMSSL)
-        }
-    }
-}
-
-val javadocJar by tasks.creating(Jar::class) {
-    archiveClassifier.value("javadoc")
-}
-
-signing {
-    setRequired {
-        !gradle.taskGraph.allTasks.any { it is PublishToMavenLocal }
-    }
-    val key = local.getProperty("signingKey") ?: System.getenv("SIGNING_KEY")
-    val password = local.getProperty("signingPassword") ?: System.getenv("SIGNING_PASSWORD")
-    if (key != null && password != null) {
-        useInMemoryPgpKeys(key, password)
-        sign(publishing.publications) // This ensures all created publications are signed
-    } else {
-        // Optional: Log a warning if keys are missing for a release build
-        logger.warn("Signing key or password not found. Publication will not be signed.")
-    }
-}
+// Root module does not publish to Maven Central
+// Only the submodules (core, engines, plugins, legacy) publish with unique artifact IDs
+// This prevents duplicate entries when publishing
